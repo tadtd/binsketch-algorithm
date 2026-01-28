87c0829021d473a05e504c11e1e9e6d8107a8d10
tadtd <tiendat1243oo@gmail.com>
2026-01-28 20:10:24 +0700

    fix: change add score postfix to metric name

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
 from .models import BinSketch, BinaryCompressionSchema, MinHash, SimHash
 from .similarity_scores import inner_product, hamming_distance, jaccard_similarity, cosine_similarity
-from .metric import mse, minus_log_mse, precision, recall, f1_score
+from .metric import mse, minus_log_mse, precision_score, recall_score, f1_score, accuracy_score
 from .utils import *
 
 __all__ = [
     'BinSketch', 'BinaryCompressionSchema', 'MinHash', 'SimHash',
     'inner_product', 'hamming_distance', 'jaccard_similarity', 'cosine_similarity',
-    'mse', 'minus_log_mse', 'precision', 'recall', 'f1_score',
+    'mse', 'minus_log_mse', 'precision_score', 'recall_score', 'f1_score', 'accuracy_score',
     'save_compression_matrix',
 ]
